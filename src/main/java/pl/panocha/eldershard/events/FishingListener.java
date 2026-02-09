f80265503a24069daf960837e9999ac49b204c3e
Maciej Panocha <maciej@panocha.pl>
2026-02-09 14:38:13 +0100

    removed unused imports

--- src/main/java/pl/panocha/eldershard/events/FishingListener.java
+++ src/main/java/pl/panocha/eldershard/events/FishingListener.java
@@ -1,13 +1,11 @@
 package pl.panocha.eldershard.events;
 
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 public class FishingListener implements Listener {
 
@@ -18,7 +16,7 @@ public class FishingListener implements Listener {
 
         ItemStack stack = item.getItemStack();
         if (stack.getType() != Material.ENCHANTED_BOOK) return;
-        
+
         stack.setType(Material.BOOK);
     }
 
