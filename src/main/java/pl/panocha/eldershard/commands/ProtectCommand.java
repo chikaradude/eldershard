b46d0eaf8532383c070679771fb0d0134aac4537
Maciej Panocha <maciej@panocha.pl>
2026-02-14 12:53:36 +0100

    better command file naming convention

--- src/main/java/pl/panocha/eldershard/commands/ProtectCommand.java
+++ src/main/java/pl/panocha/eldershard/commands/ProtectCommand.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NonNull;
 
-public class Protect implements CommandExecutor {
+public class ProtectCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command,
