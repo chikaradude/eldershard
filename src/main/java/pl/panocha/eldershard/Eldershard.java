76f471aa2f77b39e363c3a5b02ebe803ebf2dd97
Maciej Panocha <maciej@panocha.pl>
2026-03-09 01:20:12 +0100

    made all portals destination controlled

--- src/main/java/pl/panocha/eldershard/Eldershard.java
+++ src/main/java/pl/panocha/eldershard/Eldershard.java
@@ -36,6 +36,7 @@ public final class Eldershard extends JavaPlugin {
         pm.registerEvents(new BreakingListener(), this);
         pm.registerEvents(new InteractListener(), this);
         pm.registerEvents(new QuitListener(), this);
+        pm.registerEvents(new PortalListener(), this);
 
         Objects.requireNonNull(this.getCommand("test")).setExecutor(new TestCommand());
         Objects.requireNonNull(this.getCommand("openchest")).setExecutor(new OpenChestCommand());
---
+++ src/main/java/pl/panocha/eldershard/events/PortalListener.java
@@ -0,0 +1,33 @@
+package pl.panocha.eldershard.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
+
+import java.util.Objects;
+
+public class PortalListener implements Listener {
+
+    @EventHandler
+    public void onPlayerPortal(PlayerPortalEvent event) {
+        if (event.getCause() == PlayerPortalEvent.TeleportCause.NETHER_PORTAL) {
+            event.setCancelled(true);
+
+            World toWorld = (Objects.requireNonNull(
+                    event.getFrom().getWorld()).getEnvironment() == World.Environment.NETHER)
+                    ? Bukkit.getWorld("eldershard")
+                    : Bukkit.getWorld("eldershard_nether");
+
+            if (toWorld == null) return;
+
+            Location target = (toWorld.getEnvironment() == World.Environment.NETHER)
+                    ? new Location(toWorld, 0.5, 202, 0.5)
+                    : new Location(toWorld, 100.5, 65, 100.5); //todo nether
+
+            event.getPlayer().teleport(target);
+        }
+    }
+}
