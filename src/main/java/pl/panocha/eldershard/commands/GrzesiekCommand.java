b1dcb36361a49a325275ae1a33c3668eb98650ec
Maciej Panocha <maciej@panocha.pl>
2026-03-13 01:57:38 +0100

    gave grzesiek first final trades

--- src/main/java/pl/panocha/eldershard/commands/GrzesiekCommand.java
+++ src/main/java/pl/panocha/eldershard/commands/GrzesiekCommand.java
@@ -18,20 +18,39 @@ public class GrzesiekCommand implements CommandExecutor {
         Merchant merchant = Bukkit.createMerchant();
         List<MerchantRecipe> recipes = new ArrayList<>();
 
-        // TRADE 1: 10 emeraldów za diament
-        MerchantRecipe trade1 = new MerchantRecipe(new ItemStack(Material.DIAMOND), 9999);
-        trade1.addIngredient(new ItemStack(Material.EMERALD, 10));
+        // SKUP - gracz dostaje za to emeraldy
+        MerchantRecipe trade1 = new MerchantRecipe(new ItemStack(Material.EMERALD), 999); //to zyskujesz
+        trade1.addIngredient(new ItemStack(Material.COAL, 64)); //to dajesz wiesniakowi
         recipes.add(trade1);
-
-        // TRADE 2: 5 emeraldów za golden apple
-        MerchantRecipe trade2 = new MerchantRecipe(new ItemStack(Material.GOLDEN_APPLE), 9999);
-        trade2.addIngredient(new ItemStack(Material.EMERALD, 5));
+        MerchantRecipe trade2 = new MerchantRecipe(new ItemStack(Material.EMERALD), 999); //to zyskujesz
+        trade2.addIngredient(new ItemStack(Material.IRON_INGOT, 16)); //to dajesz wiesniakowi
         recipes.add(trade2);
-
-        // TRADE 3: 32 stone za 1 emerald
-        MerchantRecipe trade3 = new MerchantRecipe(new ItemStack(Material.EMERALD), 9999);
-        trade3.addIngredient(new ItemStack(Material.STONE, 32));
+        MerchantRecipe trade4 = new MerchantRecipe(new ItemStack(Material.EMERALD), 999); //to zyskujesz
+        trade4.addIngredient(new ItemStack(Material.GOLD_INGOT, 32)); //to dajesz wiesniakowi
+        recipes.add(trade4);
+        MerchantRecipe trade3 = new MerchantRecipe(new ItemStack(Material.EMERALD), 999); //to zyskujesz
+        trade3.addIngredient(new ItemStack(Material.GLASS, 16)); //to dajesz wiesniakowi
         recipes.add(trade3);
+        MerchantRecipe trade5 = new MerchantRecipe(new ItemStack(Material.EMERALD), 999); //to zyskujesz
+        trade5.addIngredient(new ItemStack(Material.WHITE_WOOL, 32)); //to dajesz wiesniakowi
+        recipes.add(trade5);
+
+        //SPRZEDAZ - wiesniak sprzedaje to graczom
+        MerchantRecipe trade6 = new MerchantRecipe(new ItemStack(Material.BRICK, 64), 999); //to zyskujesz
+        trade6.addIngredient(new ItemStack(Material.EMERALD, 4)); //to dajesz wiesniakowi
+        recipes.add(trade6);
+        MerchantRecipe trade7 = new MerchantRecipe(new ItemStack(Material.GLASS, 8), 999); //to zyskujesz
+        trade7.addIngredient(new ItemStack(Material.EMERALD, 1)); //to dajesz wiesniakowi
+        recipes.add(trade7);
+        MerchantRecipe trade8 = new MerchantRecipe(new ItemStack(Material.LANTERN, 1), 999); //to zyskujesz
+        trade8.addIngredient(new ItemStack(Material.EMERALD, 1)); //to dajesz wiesniakowi
+        recipes.add(trade8);
+        MerchantRecipe trade9 = new MerchantRecipe(new ItemStack(Material.CAMPFIRE, 1), 999); //to zyskujesz
+        trade9.addIngredient(new ItemStack(Material.EMERALD, 1)); //to dajesz wiesniakowi
+        recipes.add(trade9);
+        MerchantRecipe trade10 = new MerchantRecipe(new ItemStack(Material.EXPERIENCE_BOTTLE, 4), 999); //to zyskujesz
+        trade10.addIngredient(new ItemStack(Material.EMERALD, 1)); //to dajesz wiesniakowi
+        recipes.add(trade10);
 
         merchant.setRecipes(recipes);
 
